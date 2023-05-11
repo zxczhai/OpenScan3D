948c1163f425b7ff29aef7c2ce9e23df099de2ab
zxc <luthecatz@gmail.com>
2023-05-11 23:49:15 +0800

    merge embed UI

--- build/CMakeFiles/OpenScan3D_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/OpenScan3D_autogen"
-  "OpenScan3D_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/OpenScan3D_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
